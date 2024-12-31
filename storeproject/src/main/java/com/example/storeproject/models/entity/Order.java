747096aa729cd8acea820a614d073c1b7d90b250
JustineJensen <justinejensen1@gmail.com>
2025-01-01 00:18:31 +0100

    added order entity with fields

---
+++ storeproject/src/main/java/com/example/storeproject/models/entity/Order.java
@@ -0,0 +1,33 @@
+package com.example.storeproject.models.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.mapstruct.control.MappingControl;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id",nullable = false)
+    @Schema(description = "manager of the employee")
+    private User user;
+
+    @Column(name = "order_date",nullable = false)
+    @Schema(description = "Order date",example = "2024-02-12")
+    private LocalDateTime orderDate;
+
+    @Schema(description = " Total price of the products")
+    @Column(name="total_price")
+    private double totalPrice;
+
+}
